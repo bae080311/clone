a0a4af781a3c977d19d18702dab30d6f02cc47d3
bae080311 <156505831+bae080311@users.noreply.github.com>
2024-05-12 07:09:11 +0900

    check

--- app.js
+++ app.js
@@ -1,21 +1,59 @@
-function selectAll(selectAll)  {
-    const checkboxes = document.querySelectorAll('#agree input[type="checkbox"]');
-    checkboxes.forEach((checkbox) => {
-    checkbox.checked = selectAll.checked;
-});
-}
-function checkIt(){
-    var email = userinput.email.value
-    var exptext = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+function emailCheck(email) {
 
-        if(exptext.test(email)==false){
-			
-
-        alert("이메일형식이 올바르지 않습니다.");
-
-        userinput.email.focus();
-
-    return false;
-        }
+    var email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
     
+    return email_regex.test(email);
+}
+
+function checkEmail() {
+    var emailInput = document.getElementById('email');
+    var resultDiv = document.getElementById('result');
+
+    var email = emailInput.value;
+
+    if (emailCheck(email)) {
+        resultDiv.innerHTML = '유효한 이메일 주소입니다.';
+    } else {
+        resultDiv.innerHTML = '유효하지 않은 이메일 주소입니다.';
+    }
+}
+   
+function passwordCheck(password) {
+    
+    var password_regex = /^(?=.*\d)(?=.*[a-z])(?=.*[!@#$%^&*()_+}{"':;?/>.<,]).{8,}$/;
+    
+   
+    return password_regex.test(password);
+}
+
+function checkPassword() {
+    var passwordInput = document.getElementById('password');
+    var resultDiv = document.getElementById('result1');
+
+    var password = passwordInput.value;
+
+    if (passwordCheck(password)) {
+        resultDiv.innerHTML = '유효한 비밀번호입니다.';
+    } else {
+        resultDiv.innerHTML = '비밀번호는 최소 8자 이상, 최소 하나의 숫자, 하나의 소문자, 하나의 특수 문자를 포함해야 합니다.';
+    }
+}
+function passwordMatch(password, confirmPassword) {
+
+    return password === confirmPassword;
+}
+
+function checkcheckPassword() {
+    var passwordInput = document.getElementById('password');
+    var confirmPasswordInput = document.getElementById('confirmPassword');
+    var resultDiv = document.getElementById('result2');
+
+    var password = passwordInput.value;
+    var confirmPassword = confirmPasswordInput.value;
+
+    if (passwordMatch(password, confirmPassword)) {
+        resultDiv.innerHTML = '비밀번호가 일치합니다.';
+    } else {
+        resultDiv.innerHTML = '비밀번호가 일치하지 않습니다.';
+    }
 }
